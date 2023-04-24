4e99eeb015666b3ed3f9a0aeb377524de09bd15c
akku700 <akku29168@gmail.com>
2023-04-24 19:59:58 +0530

    initial commit

--- controllers/likeandunlikeController.js
+++ controllers/likeandunlikeController.js
@@ -2,8 +2,8 @@ const Like = require("../models/likeProduct");
 const UnLike = require("../models/unlikeProduct");
 const AsyncHandler = require("express-async-handler");
 const AppError = require(".././error/AppError");
-const Product = require("../models/productModel");
-const { query } = require("express");
+const Product = require("../models/Product");
+const expressAsyncHandler = require("express-async-handler");
 
 const likeproduct = AsyncHandler(async (req, res, next) => {
   try {
@@ -64,33 +64,98 @@ const likeproduct = AsyncHandler(async (req, res, next) => {
   }
 });
 
-const mostlike = async (req, res, next) => {
-  const query = req.query.limit || 10;
-  if (isNaN(query)) return next(new AppError("query must be a number"));
+// const mostlike = async (req, res, next) => {
+//   const query = req.query.limit || 10;
+//   if (isNaN(query)) return next(new AppError("query must be a number"));
 
-  const data = await Like.aggregate([
-    {
-      $group: {
-        _id: "$product_id",
-        count: { $sum: 1 },
+//   const data = await Like.aggregate([
+//     {
+//       $group: {
+//         _id: "$product_id",
+//         count: { $sum: 1 },
+//       },
+//     },
+//     {
+//       $sort: { count: -1 },
+//     },
+//   ]).exec();
+//   if (data.length > 1) {
+//     res.status(200).json({
+//       status: "success",
+//       data,
+//     });
+//   } else {
+//     res.status(200).json({
+//       status: "success",
+//       data,
+//     });
+//   }
+//   console.log(data);
+// };
+
+const mostlike = expressAsyncHandler(async (req, res, next) => {
+  try {
+    const likedbikes = await Like.aggregate([
+      {
+        $group: {
+          _id: "$product_id",
+          count: { $sum: 1 }, // counting no. of documents pass
+        },
       },
-    },
-    {
-      $sort: { count: -1 },
-    },
-  ]).exec();
-  if (data.length > 1) {
-    res.status(200).json({
-      status: "success",
-      data,
-    });
-  } else {
-    res.status(200).json({
-      status: "success",
-      data,
+      {
+        $sort: { count: -1 },
+      },
+    ]).exec();
+
+    if (likedbikes.length == 0) {
+      return next(new AppError("Not found any like on bike", 404));
+    }
+
+    const mostlikeCount = likedbikes[0].count;
+    console.log(mostlikeCount);
+
+    const bikeIds = likedbikes.map((bike) => {
+      if (mostlikeCount === bike.count) {
+        return bike._id;
+      }
+    }); // Extract bike IDs from aggregation result
+    const mostlikedbikes = await Product.find({ _id: { $in: bikeIds } }) // Find bikes with matching IDs
+      .populate({
+        path: "likes",
+        select: "user_id -bike_id -_id",
+      })
+      .populate({
+        path: "likescount",
+      })
+      .populate({
+        path: "dislikes",
+        select: "user_id -bike_id -_id",
+      })
+      .populate({
+        path: "dislikescount",
+      })
+      .populate({
+        path: "comments",
+        select: "comment  -bike_id -_id",
+      })
+      .populate({
+        path: "commentscount",
+      });
+
+    if (mostlikedbikes) {
+      res.json({
+        data: mostlikedbikes,
+      });
+    } else {
+      return next(new AppError("Bikes are not found", 404));
+    }
+  } catch (err) {
+    console.log("Most recent bike fetching Error", err);
+    res.status(404).json({
+      status: false,
+      message: "Failed to load most liked bikes",
     });
   }
-  console.log(data);
-};
+});
 
 module.exports = { likeproduct, mostlike };
